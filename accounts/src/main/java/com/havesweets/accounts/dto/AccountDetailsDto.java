2038bd7b882e7c8a0415eb598cc7107d691ee2cf
prashanth <prashanthchinta29@gmail.com>
2024-08-01 13:23:17 +0530

    for jenkins check

--- accounts/src/main/java/com/havesweets/accounts/dto/AccountDetailsDto.java
+++ accounts/src/main/java/com/havesweets/accounts/dto/AccountDetailsDto.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class AccountDetailsDto {
     private AccountsDto accountsDto;
     private List<CardsDto> cardsDtos;
